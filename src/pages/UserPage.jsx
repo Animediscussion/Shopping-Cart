321528f94beb855da9da6f4f1308563c1d78ef4f
Animediscussion <prasan1010jeet@gmail.com>
2026-06-07 04:49:07 +0530

    update

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  NavLink,
+} from "react-router-dom";
+import HomePage from "./HomePage";
+import CartPage from "./CartPage";
+import UserPage from "./UserPage";
 
 const UserPage = () => {
-  return <div>UserPage</div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/user" element={<UserPage />} />
+        </Routes>
+      </Router>
+    </>
+  );
 };
 
 export default UserPage;
