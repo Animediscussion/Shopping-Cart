e5e9b526f355cfb1bf03b5e39d434758384403db
Animediscussion <prasan1010jeet@gmail.com>
2026-06-08 00:57:07 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -13,16 +13,35 @@ import UserPage from "./pages/UserPage";
 function App() {
   return (
     <>
-      <Router>
-        <div className="min-h-screen transistion-colors bg-gray-900 text-white">
-          Shopping App
-        </div>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/user" element={<UserPage />} />
-        </Routes>
-      </Router>
+      <div className="min-h-screen bg-gray-100 text-gray-900">
+        <Router>
+          <div className="p-4 shadow-md flex justify-between items-center bg-white dark:bg-gray-800">
+            <NavLink to="/" className="text-xl font-bold dark:text-white">
+              🛒Shopping App
+            </NavLink>
+            <div className="flex items-center gap-4">
+              <NavLink
+                to="/cart"
+                className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded transition"
+              >
+                View Cart
+              </NavLink>
+              <NavLink
+                to="/user"
+                className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded transition"
+              >
+                User Info
+              </NavLink>
+            </div>
+          </div>
+
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/user" element={<UserPage />} />
+          </Routes>
+        </Router>
+      </div>
     </>
   );
 }
