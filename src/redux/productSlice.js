eaa377c66c88e8ffa63e27870afea2506f01cdce
Animediscussion <prasan1010jeet@gmail.com>
2026-06-14 22:32:50 +0530

    update

--- src/redux/productSlice.js
+++ src/redux/productSlice.js
@@ -3,6 +3,25 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 const fetchProductsThunk = createAsyncThunk({});
 const productSlice = createSlice({
   name: "products",
+  initialState: {
+    items: [],
+    loading: false,
+    error: false,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchProductsThunk.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchProductsThunk.fulfilled, (state, action) => {
+      state.loading = false;
+      state.items = action.payload;
+    });
+    builder.addCase(fetchProductsThunk.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.error.message;
+    });
+  },
 });
 
 export default productSlice;
