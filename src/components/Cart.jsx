d2a458d5206f64034da9f64c4c782286049e28f9
Animediscussion <prasan1010jeet@gmail.com>
2026-06-16 19:52:41 +0530

    update

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Cart = () => {
+  const cartItems = useSelector((state) => state.cart?.cartItems);
+  const isDarkMode = true;
   return <div>Cart</div>;
 };
 
