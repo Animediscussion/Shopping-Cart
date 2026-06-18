960651d6356f39a9153a20c4ad81385221731d23
Animediscussion <prasan1010jeet@gmail.com>
2026-06-19 03:16:17 +0530

    persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,12 @@ import userReducer from "./userSlice";
 import productReducer from "./productSlice";
 import cartReducer from "./cartSlice";
 import themeReducer from "./themeSlice";
+import storage from "redux-persist/lib/storage";
+
+const cartPersistConfig = {
+  key: "cart",
+  storage: storage,
+};
 
 const store = configureStore({
   reducer: {
